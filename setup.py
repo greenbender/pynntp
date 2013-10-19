1f0da2d6366abde2cb8bdac23268eb22b63fc81b
greenbender <byron.platt@gmail.com>
2013-10-19 19:17:26 +1100

    0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pynntp',
     version='0.8',
-    description='Python NNTP Library (including compressed headers)',
+    description='NNTP Library (including compressed headers)',
     author='Byron Platt',
     author_email='byron.platt@gmail.com',
     url='https://github.com/greenbender/pynntp',
