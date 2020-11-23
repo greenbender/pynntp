9a13b961cae6f6c6801829b2da8a818da4b23e13
greenbender <byron.platt@gmail.com>
2020-11-23 18:31:32 +1100

    Actually use README.md for pypi description
    Bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+import os
+
+
+# read the contents of your README file
+path = os.path.abspath(path.dirname(__file__))
+with open(os.path.join(path, 'README.md'), encoding='utf-8') as fd:
+    long_description = fd.read()
 
 
 setup(
     name='pynntp',
-    version='1.0.1',
+    version='1.0.2',
     description='NNTP Library (including compressed headers)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Byron Platt',
     author_email='byron.platt@gmail.com',
     license='GPL3',
